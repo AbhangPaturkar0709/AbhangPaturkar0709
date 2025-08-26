535b4d961ffe7a9efc72383e21b3640efac3914b
Abhang Paturkar <abhangpaturkar0709@gmail.com>
2025-08-26 23:02:54 +0530

    Project added

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: './', // 👈 REQUIRED for GitHub Pages
+  base: "/AbhangPaturkar0709/",  
   server: {
     host: "::",
     port: 8080,
